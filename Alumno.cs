1af4e0979c9b4b223aa4f35d0423b9ac04ca13df
Ortizcarmen <156687528+Ortizcarmen@users.noreply.github.com>
2024-05-06 09:31:22 -0600

    Agregar archivos de proyecto.

---
+++ Alumno.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Prueba
+{
+    public class Alumno : Persona
+    {
+        public string NumeroCarne { get; set; }
+        public List<int> Notas { get; set; }
+
+        public Alumno()
+        {
+            NumeroCarne = string.Empty;
+            Notas = new List<int>();
+        }
+    }
+}
---
+++ Catedratico.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Prueba
+{
+    public class Catedratico:Persona
+    {
+        [Required(ErrorMessage = "El Título es Requerido")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "El Colegiado es Requerido")]
+        [StringLength(6, ErrorMessage = "Deben ser menos de 6 dígitos")]
+        public string Colegiado { get; set; }
+
+        public Catedratico()
+        {
+            Titulo = string.Empty;
+            Colegiado = string.Empty;
+        }
+    }
+}
